32025ffd55e23876e152c5a58ba32c2114157bed
Tom Bremer <tom@tbremer.com>
2020-03-19 14:54:53 -0500

    make a robot do it

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 # ls -la
-echo "test" > ./scripts/test.txt
+echo "beepbeep boop" > ./scripts/test.txt
+
+git config --global user.email "actions@github.com"
+git config --global user.name "Robot Downloader"
+
 git add ./scripts/test.txt
 git commit -m "robots doing work"
 git push
